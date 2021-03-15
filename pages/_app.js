d7c397e29c1653bab4851afa8b487f2c719b41df
Neel Gaywala <neelgaywala72@gmail.com>
2021-03-15 20:30:22 +0530

    day 1, page ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
+import React, {useEffect} from 'react';
+import '../styles/styles.scss';
+import "slick-carousel/slick/slick.scss";
+import "slick-carousel/slick/slick-theme.scss";
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if(router.pathname === '/') {
+      router.push('/home', '/home');
+    }
+  }, [])
+  // 
+  // router.push('/home');
+
   return <Component {...pageProps} />
 }
 
